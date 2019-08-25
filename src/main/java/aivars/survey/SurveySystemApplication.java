eee6f315a4a8acf1b01b575131decfd5448e5722
Aivars Stepiņš <aimis.g1@gmail.com>
2019-08-25 23:00:07 +0300

    update main application class - add mvc bean for cors config

--- src/main/java/aivars/survey/SurveySystemApplication.java
+++ src/main/java/aivars/survey/SurveySystemApplication.java
@@ -2,6 +2,11 @@ package aivars.survey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.beans.BeanProperty;
 
 @SpringBootApplication
 public class SurveySystemApplication {
@@ -9,5 +14,17 @@ public class SurveySystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SurveySystemApplication.class, args);
     }
+
+    @Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+    	return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedMethods("DELETE", "GET", "POST", "PUT")
+					.allowedOrigins("*");
+			}
+		};
+	}
 
 }
